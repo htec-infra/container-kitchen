7be18e14e86847766ed164c46cd3bc4be18f2f95
Nikola Grabovica <nikola.grabovica@htecgroup.com>
2021-11-10 09:50:19 +0100

    docker-ops logic updated Makefule updated README.md updated

--- infra/docker-ops.sh
+++ infra/docker-ops.sh
@@ -1,42 +1,91 @@
 #!/bin/bash
 
-IMAGE_NAME=${1}
-IMAGE_NAME_REDUCED=${IMAGE_NAME%/*}
-# shellcheck disable=SC2210
-IMAGE_VERSION=$(cat apps/"${IMAGE_NAME}"/VERSION 2>/dev/null || echo "${2}")
+# Input vars
+REPOSITORY_NAME=${1}
+FLAVOR_NAME=${2} # optional
 
-LOCAL_IMAGE="local/${IMAGE_NAME_REDUCED}:${IMAGE_VERSION}"
-echo $LOCAL_IMAGE
-# We want to push same images on multiple docker registries
-DOCKER_REPOS=("public.ecr.aws/htec")
+# Static vars
+DOCKER_REPOS=("public.ecr.aws/htec" "docker.hub/htec" "eu.gcr.io/htec")
+BASE_DIR="$PWD/apps"
+FULL_PATH=$BASE_DIR/$REPOSITORY_NAME/$FLAVOR_NAME
+SUBDIRS=$(find ${FULL_PATH} -maxdepth 1 -type d | wc -l)
+IMAGE_VERSION=$(cat "${FULL_PATH}"/VERSION 2>/dev/null || echo "${3}")
 
-build() {
-  docker build -t "${LOCAL_IMAGE}" --build-arg "VERSION=${IMAGE_VERSION}" "apps/${IMAGE_NAME}/"
-}
 
-push() {
-  if [ "${LOCAL_TEST}" == "true" ]; then
-      echo "Skipping 'docker push' for local testing."
-      return
+
+main(){
+  if [ -z "$REPOSITORY_NAME" ] ; then
+      echo "No mandatory argument supplied"
+    else
+      for REPO in "${DOCKER_REPOS[@]}"; do
+        build_and_push
+      done
   fi
-  for REPO in "${DOCKER_REPOS[@]}"; do
-    NEW_IMAGE="$REPO/${IMAGE_NAME}:${IMAGE_VERSION}"
-    docker tag "local/${IMAGE_NAME}:${IMAGE_VERSION}" "${NEW_IMAGE}"
-    docker push "${NEW_IMAGE}"
-
-    # Remove tag after push
-    docker rmi "${NEW_IMAGE}"
-  done
 }
 
-cleanup() {
-  if [ "${LOCAL_TEST}" == "true" ]; then
-      echo "Skipping cleanup"
+
+# Functions
+build_and_push() {
+    if [[ "${SUBDIRS}" -eq 1 ]]
+    then
+      for FOLDER in $(ls -d ${FULL_PATH}); do
+        IMAGE_NAME=$REPO/${REPOSITORY_NAME}:${IMAGE_VERSION}
+        build_image $DRY_RUN $IMAGE_NAME $FOLDER
+        push_image $DRY_RUN $IMAGE_NAME
+        remove_image $DRY_RUN $IMAGE_NAME
+      done
+    else
+      for FOLDER in $(ls -d ${FULL_PATH}/*/); do
+        if [ -f "Dockerfile" ]; then
+          IMAGE_NAME=$REPO/${REPOSITORY_NAME}:${IMAGE_VERSION}-$(basename "$FOLDER/")
+          build_image $DRY_RUN $IMAGE_NAME $FOLDER
+          push_image $DRY_RUN $IMAGE_NAME
+          remove_image $DRY_RUN $IMAGE_NAME
+        else
+          FOLDER=$(ls -d ${FULL_PATH})
+          IMAGE_NAME=$REPO/${REPOSITORY_NAME}:${IMAGE_VERSION}
+          build_image $DRY_RUN $IMAGE_NAME $FOLDER
+          push_image $DRY_RUN $IMAGE_NAME
+          remove_image $DRY_RUN $IMAGE_NAME
+        fi
+      done
+    fi
+}
+
+build_image(){
+  local DRY_RUN="$1"
+  local IMAGE_NAME="$2"
+  local FOLDER="$3"
+
+  if [ "$DRY_RUN" = "true" ] ; then
+    docker build -t local/$IMAGE_NAME "$FOLDER/"
   else
-      docker rmi "${LOCAL_IMAGE}"
-  fi;
+    docker build -t $IMAGE_NAME "$FOLDER/"
+  fi
+}
+
+push_image(){
+  local DRY_RUN="$1"
+  local IMAGE_NAME="$2"
+
+  if [ "$DRY_RUN" = "true" ] ; then
+    echo "Push disabled"
+  else
+    echo docker push -t $IMAGE_NAME
+  fi
+}
+
+remove_image(){
+
+  local DRY_RUN="$1"
+  local IMAGE_NAME="$2"
+
+  if [ "$DRY_RUN" = "true" ] ; then
+    echo "Cleanup skipped"
+  else
+    docker rmi $IMAGE_NAME
+  fi
 
 }
 
-build && push && cleanup
-
+main
