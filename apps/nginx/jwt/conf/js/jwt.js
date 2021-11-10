07e3309e2b06b905a9fc02a9813ed584d69e6943
Jovica Zlatanovic <jovica.zlatanovic@htecgroup.com>
2021-11-10 18:17:12 +0100

    Nginx with JWT verification

---
+++ apps/nginx/jwt/conf/js/jwt.js
@@ -0,0 +1,12 @@
+function jwt(data) {
+  var parts = data.split('.').slice(0,2)
+    .map(v=>Buffer.from(v, 'base64url').toString())
+    .map(JSON.parse);
+  return { headers:parts[0], payload: parts[1] };
+}
+
+function jwt_payload_sub(r) {
+  return jwt(r.headersIn.Authorization.slice(7)).payload.sub;
+}
+
+export default {jwt_payload_sub}
